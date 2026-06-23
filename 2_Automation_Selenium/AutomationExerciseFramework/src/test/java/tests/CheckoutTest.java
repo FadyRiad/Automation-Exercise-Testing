30ccd7a2c8954c28fb67bb35336cb9c53aabe1eb
fady riad <91222856+FadyRiad@users.noreply.github.com>
2026-06-23 23:55:16 +0300

    chore: sync latest project structure and gitignore

--- 2_Automation_Selenium/AutomationExerciseFramework/src/test/java/tests/CheckoutTest.java
+++ 2_Automation_Selenium/AutomationExerciseFramework/src/test/java/tests/CheckoutTest.java
@@ -12,17 +12,13 @@ public class CheckoutTest extends BaseTest {
         CartPage cartPage = new CartPage(driver);
         CheckoutPage checkoutPage = new CheckoutPage(driver);
 
-        // 1- add product
         driver.navigate().to("https://automationexercise.com/products");
         productsPage.addFirstProduct();
 
-        // 2- view cart
         productsPage.clickViewCart();
 
-        // 3- checkout
         cartPage.clickProceedToCheckout();
 
-        // 4- register/login
         checkoutPage.clickRegisterLogin();
 
 
